328df65f7cc43ec63a7b98cda54d6ff45f044855
pawel <pawel@pawel-R520-R522-R620.(none)>
2013-08-04 19:53:53 +0200

    bluez part5

--- core/java/android/server/BluetoothService.java
+++ core/java/android/server/BluetoothService.java
@@ -1735,17 +1735,18 @@ public class BluetoothService extends IBluetooth.Stub {
         }
     };
 
-    private void registerForAirplaneMode(IntentFilter filter) {
+    @SuppressWarnings("deprecation")
+	private void registerForAirplaneMode(IntentFilter filter) {
         final ContentResolver resolver = mContext.getContentResolver();
         final String airplaneModeRadios = Settings.System.getString(resolver,
-                Settings.System.AIRPLANE_MODE_RADIOS);
+                Settings.Global.AIRPLANE_MODE_RADIOS);
         final String toggleableRadios = Settings.System.getString(resolver,
-                Settings.System.AIRPLANE_MODE_TOGGLEABLE_RADIOS);
+                Settings.Global.AIRPLANE_MODE_TOGGLEABLE_RADIOS);
 
         mIsAirplaneSensitive = airplaneModeRadios == null ? true :
-                airplaneModeRadios.contains(Settings.System.RADIO_BLUETOOTH);
+                airplaneModeRadios.contains(Settings.Global.RADIO_BLUETOOTH);
         mIsAirplaneToggleable = toggleableRadios == null ? false :
-                toggleableRadios.contains(Settings.System.RADIO_BLUETOOTH);
+                toggleableRadios.contains(Settings.Global.RADIO_BLUETOOTH);
 
         if (mIsAirplaneSensitive) {
             filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
@@ -1755,7 +1756,7 @@ public class BluetoothService extends IBluetooth.Stub {
     /* Returns true if airplane mode is currently on */
     /*package*/ final boolean isAirplaneModeOn() {
         return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.AIRPLANE_MODE_ON, 0) == 1;
+                Settings.Global.AIRPLANE_MODE_ON, 0) == 1;
     }
 
     /* Broadcast the Uuid intent */
@@ -2758,10 +2759,11 @@ public class BluetoothService extends IBluetooth.Stub {
     private void readIncomingConnectionState() {
         synchronized(mIncomingConnections) {
             FileInputStream fstream = null;
+            BufferedReader file = null;
             try {
               fstream = new FileInputStream(INCOMING_CONNECTION_FILE);
               DataInputStream in = new DataInputStream(fstream);
-              BufferedReader file = new BufferedReader(new InputStreamReader(in));
+              file = new BufferedReader(new InputStreamReader(in));
               String line;
               while((line = file.readLine()) != null) {
                   line = line.trim();
@@ -2784,6 +2786,13 @@ public class BluetoothService extends IBluetooth.Stub {
                     } catch (IOException e) {
                         // Ignore
                     }
+                }
+                if (file != null) {
+                	try {
+                		file.close();
+                	} catch (IOException e) {
+                		// Ignore
+                	}
                 }
             }
         }
